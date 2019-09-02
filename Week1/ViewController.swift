bd22a3038e57aa9308eda8256acc08137645f0ec
Andrew W King <kinglx@bc.edu>
2019-09-02 13:35:04 -0400

    videos 2.1-2.9

--- Week1/ViewController.swift
+++ Week1/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sayItButtonPressed(_ sender: UIButton) {
+        let messages = ["You are Awesome!", "You are Great!", "You are Amazing!"]
+        
+        if messageLabel.text == messages[0] {
+            messageLabel.text = messages[1]
+            messageLabel.textColor = UIColor.red
+        } else if messageLabel.text == messages[1] {
+            messageLabel.text = messages[2]
+            messageLabel.textColor = UIColor.blue
+        } else {
+            messageLabel.text = messages[0]
+            messageLabel.textColor = UIColor.green
+        }
+    }
 }
-
+ 
